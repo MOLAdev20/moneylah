839326bf1b24f1d9e5853a465a4bb1f223466001
sabilul9090 <Billdev@gmail.com>
2020-07-15 18:50:48 +0700

    First CRUD

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -12,12 +12,58 @@ class Home extends CI_Controller {
 		$this->load->model('Home_model');
 	}
 
+	protected function form_confirmation()
+	{
+		$this->form_validation->set_rules("pemasukan", "pemasukan", "required");
+		$this->form_validation->set_rules("nominal", "nominal", "required");
+	}
+
 	public function index()
 	{
 		// Load tampilan dari controller yang sudah di modifikasi
-		parent::getView("dashboard");
+		$data['pemasukan'] = $this->Home_model->showData();
+		parent::getView("dashboard", $data);
+
 	}
 
+	public function create()
+	{
+		$this->form_confirmation();
+
+		if ($this->form_validation->run() == TRUE) {
+			$this->Home_model->add();
+		}else{
+			$this->session->set_flashdata("form", "<div class='alert bg-danger' role='alert'>Semua Form wajib di isi</div>");
+			redirect('Home/index','refresh');
+		}
+		show_404();
+	}
+
+	public function delete($id = []){
+
+		if (!empty($id)) {
+			$id = decrypt_url($id);
+			$this->Home_model->delete($id);
+		}else{
+			show_404();
+		}
+		
+	}
+
+	public function update($id)
+	{
+		$this->form_confirmation();
+
+		if (!empty($id)) {
+			$id = decrypt_url($id);
+			$this->Home_model->update($id);
+		}
+	}
+
+	public function debug()
+	{
+		$this->db->truncate("data_pemasukan");
+	}
 }
 
 /* End of file Welcome.php */
